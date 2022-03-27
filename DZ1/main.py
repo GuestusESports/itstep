ffe214bcf166f68532a38a8ba6ba82066041d554
PC <pickachu.electric@gmail.com>
2022-03-27 19:38:13 +0300

    initial commit

---
+++ DZ1/main.py
@@ -0,0 +1,7 @@
+#DZ1
+
+print("Hello, dear teacher!")
+print("This is my first Homework!")
+print("I hope you feel good now!")
+print("I hope you liked it, even if it is so simple!")
+print("Goodbye!")
