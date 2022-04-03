ee413954f8c9d58290dcd734bda309faffbc00e3
PC <pickachu.electric@gmail.com>
2022-04-03 14:53:34 +0300

    initial commit

--- UROK3-4/main.py
+++ UROK3-4/main.py
@@ -18,6 +18,31 @@ class student:
         self.progress-=0.1
     def is_alive(self):
         if self.progress<-0.5:
-            print("Ви стали дед інсайдом і вас вигнали з дитячого садочку((")
+            print("Ви не попали в туалет вчасно і тепер весь дитячий садочок смердить.Вас вигнали з дитячого садочку за це((")
             self.alive=False
-        
+        elif self.gladness<=0:
+            print("Ви стали дед інсайдом((")
+            self.alive=False
+        elif self.progress > 5
+            print("Ви стали самим крутим карапузом в дитячому садочку і переходите в перший клас раніше інших 😎😎, але, по дорозі додому, впали в туалет, і, втопились там...")
+            self.alive=False
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress,2)}")
+    def live(self):
+        day = "Day" + str(day) + "of" + self.name + "Life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+        if live_cube == 1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+            self.end_of_day()
+            self.is_alive()
+        nick = Student(name="Nick")
+        for day in range(365):
+            if nick.alive == False:
+                break
+            nick.live(day)
