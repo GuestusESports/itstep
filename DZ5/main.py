72a348d8f424772cd3e5d1c160fc440b1fdf44d3
PC <pickachu.electric@gmail.com>
2022-05-01 12:21:33 +0300

    initial commit

---
+++ DZ5/main.py
@@ -0,0 +1,6 @@
+from colorama import Fore, Back, Style
+print(Fore.BLUE + 'додамо синього тексту')
+print(Back.RED + 'трохи спецій зчервоним фоном')
+print(Style.DIM + 'і трошки паприки нижнього тексту')
+print(Style.RESET_ALL)
+print('їжа не вдалась((')
